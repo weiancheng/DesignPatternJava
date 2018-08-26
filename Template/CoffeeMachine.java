359c0259b766756fb8266b213725d80fc31fc75c
Weian <d92434101@gmail.com>
2018-08-26 12:15:50 +0800

    modify coding style

--- Template/CoffeeMachine.java
+++ Template/CoffeeMachine.java
@@ -1,28 +1,28 @@
 public abstract class CoffeeMachine {
-	public CoffeeMachine() {
-	}
+    public CoffeeMachine() {
+    }
 
-	public abstract void grindBeans(String bean);
+    public abstract void grindBeans(String bean);
 
-	public abstract void pullWater(int temperature);
+    public abstract void pullWater(int temperature);
 
-	public abstract void pullMilk();
+    public abstract void pullMilk();
 
-	public abstract void addSugar();
+    public abstract void addSugar();
 
-	public abstract void cockCoffee(int second);
+    public abstract void cockCoffee(int second);
 
-	public void makeBlack() {
-		grindBeans("Arabica");
-		pullWater(100);
-		cockCoffee(30);
-	}
+    public void makeBlack() {
+        grindBeans("Arabica");
+        pullWater(100);
+        cockCoffee(30);
+    }
 
-	public void makeThreeInOne() {
-		grindBeans("coffee powder");
-		pullWater(90);
-		pullMilk();
-		addSugar();
-		cockCoffee(5);
-	}
+    public void makeThreeInOne() {
+        grindBeans("coffee powder");
+        pullWater(90);
+        pullMilk();
+        addSugar();
+        cockCoffee(5);
+    }
 }
--- Template/TestTemplate.java
+++ Template/TestTemplate.java
@@ -1,9 +1,9 @@
 public class TestTemplate {
-	public static void main(String [] arg) {
-		System.out.println("This is Template Pattern");
-		CoffeeMachine machine = new NestleMachine();
-		machine.makeThreeInOne();
-		System.out.println("next...");
-		machine.makeBlack();
-	}
+    public static void main(String [] arg) {
+        System.out.println("This is Template Pattern");
+        CoffeeMachine machine = new NestleMachine();
+        machine.makeThreeInOne();
+        System.out.println("next...");
+        machine.makeBlack();
+    }
 }
